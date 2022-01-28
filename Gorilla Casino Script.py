ac8111ed90a2c9cbe777f96d55e7bd02d0994c3a
Nevazhniy <78214911+Nevazhniy@users.noreply.github.com>
2022-01-28 11:33:04 +0300

    Add files via upload

---
+++ Gorilla Casino Script.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import webbrowser
+from random import randint
+input('Сейчас вы перейдёте по ссылке и нажмёте "Разрешить" \n чтобы получить access_token, скопируйте его из ссылки')
+webbrowser.open('https://oauth.vk.com/authorize?client_id=6121396&scope=69634&redirect_uri=https://api.vk.com/blank.html&display=page&response_type=token&revoke=1')
+access_token = input('access_token: ')
+bet = input('Ставка: ')
+firstDelay = int(input('Рандомная задержка в регионе от: '))
+secondDelay = int(input('до: '))
+url = f'https://api.vk.com/method/messages.send?user_id=-171493284&message=азино {bet}&v=5.9&access_token={access_token}'
+while True:
+    request = requests.get(url=url)
+    if 'response' in request.text:
+        print('newMessage')
+    elif 'captcha needed' in request.text:
+        print('captcha')
+        time.sleep(10)
+    time.sleep(randint(firstDelay, secondDelay))
